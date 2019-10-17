57e11280421e957ff7061fe3b985c7eb4afb92ef
Kelvin Pompey <kelvin.pompey@gmail.com>
2019-10-17 11:20:31 -0400

    Fixed naming errors

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "RxApolloClient",
     platforms: [.iOS(.v9), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
     products: [
-        .library(name: "ApolloAlamofire", targets: ["ApolloAlamofire"]),        
+        .library(name: "RxApolloClient", targets: ["RxApolloClient"]),        
     ],
     dependencies: [
       .package(url: "https://github.com/apollographql/apollo-ios.git", .exact("0.10.1")),
